1ebbdac849a372887ffb6234ca3b38b95ca1412f
Laphatradap <44722798+Laphatradap@users.noreply.github.com>
2020-03-06 12:07:17 +0100

    Add slice to date string

--- src/components/Availability/AvailabilityList.js
+++ src/components/Availability/AvailabilityList.js
@@ -10,22 +10,27 @@ class AvailabilityList extends Component {
     if (!this.props.entity) return null;
     return (
       <div>
-      <h1>{this.props.user.username}, your availability are:</h1>
-      <div>
-        {this.props.entity.map(e => (
-          <div key={e.id}>
-            <p>{e.startDate}</p>
-            <p>{e.endDate}</p>
-          </div>
-        ))}
-      </div>
+        <h1>{this.props.user.username}, your availability are:</h1>
+        <div>
+          {this.props.entity.map(e => (
+            <ul key={e.id}>
+              <li>
+                From {e.startDate.slice(0, 10)} at 
+                {e.startDate.slice(11, 16)}
+                <br></br>
+                To {e.endDate.slice(0, 10)} at 
+                {e.endDate.slice(11, 16)}
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  console.log("state of AvailabilityList", state)
+  // console.log("state of AvailabilityList", state);
   return {
     entity: state.availability,
     user: state.user
